e4e53fb183e14dde6cb861c145859dfb12d4ade2
Christos C <72927937+GPChrisdest@users.noreply.github.com>
2023-11-21 23:18:56 +0200

    swap() function
    Αλλαζει θεση δυο στηλες ( δεν δουλευει ακομα)

--- turtle.py
+++ turtle.py
@@ -1,7 +1,16 @@
-import turtle as t
+import turtle
+
+t = turtle.Turtle()
+
+class Collumn():
+    def __init__(self,height,position):
+        self.height = height
+        self.position = position
 
 def collumn(height,position):
     #canvas 700x650
+    t.hideturtle()
+    t.speed(200)
     t.penup()
     if position < 35:
         position = -700 + 31*position
@@ -24,3 +33,26 @@ def draw(height):
     t.forward(10*height)
     t.left(90)
     t.end_fill()
+
+def swap(c1,c2,l):
+    l.insert(c1.position,c2)
+    l.insert(c2.position,c1)
+    #l.pop(c2.position+1)
+    #l.pop(c2.position+2)
+
+
+l = [5,8,6,10]
+n = 0
+c = []
+for i in l:
+    c.append(Collumn(i,n))
+    n += 1
+
+for item in c:
+    collumn(item.height,item.position)
+
+swap(c[0],c[3],c)
+
+for i in c:
+    collumn(i.height,i.position)
+turtle.exitonclick()
