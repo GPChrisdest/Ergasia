068e938a76fc92a57ace06b86e15ba37f7e5b985
Christos C <achrisan2@gmail.com>
2023-11-22 18:47:29 +0200

    Συναρτηση swap που αλλαζει 2 στηλες

--- turtle1.py
+++ turtle1.py
@@ -1,16 +1,18 @@
 import turtle
+import tkinter
 
 t = turtle.Turtle()
+t.pencolor('white')
 
 class Collumn():
     def __init__(self,height,position):
         self.height = height
         self.position = position
 
-def collumn(height,position):
+def create(height,position,color='blue'):
     #canvas 700x650
     t.hideturtle()
-    t.speed(200)
+    t.speed('fastest')
     t.penup()
     if position < 35:
         position = -700 + 31*position
@@ -18,11 +20,11 @@ def collumn(height,position):
         position = 700 - 31*position
     t.goto(position,0)
 
-    draw(height)
+    draw(height,color)
 
-def draw(height):
+def draw(height,color):
     t.begin_fill()
-    t.fillcolor('blue')
+    t.fillcolor(color)
     t.pendown()
     t.forward(20)
     t.left(90)
@@ -33,15 +35,14 @@ def draw(height):
     t.forward(10*height)
     t.left(90)
     t.end_fill()
+    t.penup()
 
-def swap(c1,c2,l):
-    l.insert(c1.position,c2)
-    l.insert(c2.position,c1)
-    #l.pop(c2.position+1)
-    #l.pop(c2.position+2)
+def swap(c1,c2):
+    create(c1.height,c1.position,'white')
+    create(c2.height,c1.position)
+    create(c1.height,c2.position)
 
-
-l = [5,8,6,10]
+l = [5,8,6,30,10,25,17,23,15,9]
 n = 0
 c = []
 for i in l:
@@ -49,11 +50,8 @@ for i in l:
     n += 1
 
 for item in c:
-    collumn(item.height,item.position)
-    swap(c[0],c[3],c)
+    create(item.height,item.position)
 
+swap(c[3],c[4])
 
-for i in c:
-    collumn(i.height,i.position)
 turtle.exitonclick()
-
