4c055e7e8b71b70402e9e7d6e3ee8f385c5f2bdd
Christos C <achrisan2@gmail.com>
2023-11-23 14:29:12 +0200

    Αφαιρεση της κλασης Collumn( )
    Πλεον το κανει με μια απλη λιστα l = [...]

--- turtle1.py
+++ turtle1.py
@@ -3,11 +3,6 @@ import turtle
 t = turtle.Turtle()
 t.pencolor('white')
 
-class Collumn():
-    def __init__(self,height,position):
-        self.height = height
-        self.position = position
-
 def create(height,position,color='blue'):
     #canvas 700x650
     t.hideturtle()
@@ -37,19 +32,17 @@ def draw(height,color):
     t.penup()
 
 def swap(c1,c2):
-    create(c1.height,c1.position,'white')
-    create(c2.height,c1.position)
-    create(c1.height,c2.position)
+    create(c1,i-1,'white')
+    create(c2,i-1)
+    create(c1,i)
 
 l = [5,8,6,30,10,25,17,23,15,9]
-n = 0
-c = []
-for i in l:
-    c.append(Collumn(i,n))
-    n += 1
-for item in c:
-    create(item.height,item.position)
+pos = 0
 
-swap(c[3],c[4])
+for item in l:
+    create(item,pos)
+    pos+=1
+i = 4
+swap(l[i-1],l[i])
 
 turtle.exitonclick()
