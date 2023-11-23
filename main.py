87bf3da06408e277f326558acc5e021a80363271
George Haidos <haidos.george0@gmail.com>
2023-11-23 14:13:07 +0200

    Add function to clear terminal contents and fix empty input handling

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import os
 import random
 from time import sleep
 
 from insertion_sort import sort
 
 
+def clear_terminal():
+    if os.name == "nt":
+        os.system("cls")
+    elif os.name == "posix":
+        os.system("clear")
+
+
 def list_options():
     list = []
     while True:
+        clear_terminal()
         print("List Options:\n1. Generate random list\n2. Insert list\n3. View List")
         user_choice = input("Select an option: ")
-        if (
-            (not user_choice.isdigit() and user_choice != "")
-            or int(user_choice) < 1
-            or int(user_choice) > 3
-        ):
+        if user_choice == "":
+            break  # Πρέπει να δούμε πώς θέλουμε να ελέγχουμε αν ήδη υπάρχει έγκυρη λίστα στην main
+        elif not user_choice.isdigit() or int(user_choice) < 1 or int(user_choice) > 3:
             print("Invalid Choice!")
             continue
         user_choice = int(user_choice)
@@ -29,7 +36,6 @@ def list_options():
                 user_input = input(f"Element No.{i+1} ")
                 if not user_input.isdigit():
                     print("Please input a number!")
-                    i=i
                     continue
                 list.append(int(user_input))
                 i += 1
@@ -41,17 +47,18 @@ def list_options():
             print("Press enter to return to previous menu")
             input()
             continue
-        else:
-            break  # Πρέπει να δούμε πώς θέλουμε να ελέγχουμε αν ήδη υπάρχει έγκυρη λίστα στην main
     return list
 
 
 def sort_menu(list):
     user_choice = 0
     while True:
+        clear_terminal()
         print("Start Sorting:\n1. Plain sort\n2. Sort + Illustration")
         user_choice = input("Select an option: ")
-        if not user_choice.isdigit() or int(user_choice) < 1 or int(user_choice) > 2:
+        if user_choice == "":
+            break
+        elif not user_choice.isdigit() or int(user_choice) < 1 or int(user_choice) > 2:
             print("Invalid Choice!")
             continue
         user_choice = int(user_choice)
@@ -70,6 +77,7 @@ def sort_menu(list):
 def main():
     list = []
     while True:
+        clear_terminal()
         print("1. List Options\n2. Start Sorting\n3. Exit")
         user_choice = input("Select and option: ")
         if not user_choice.isdigit or int(user_choice) < 1 or int(user_choice) > 3:
