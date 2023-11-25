9e7a5c9a0684c3a6921d3cb78402d1b999116ae2
George Haidos <haidos.george0@gmail.com>
2023-11-25 22:56:44 +0200

    WIP Initial Animation

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,17 +1,23 @@
 def sort(list: list):
+    max_height = max(map(lambda c: c.get_height(), list))
     i = 1  # Starting index
     while i < len(list):  # Loop through the whole list
         for j in range(i - 1, -1, -1):  # Loop through indices below current index
-            """If the previous item of the list is smaller than the current one(i), check if the one before that(j-1)
-            is lower as well. If so, continue so that the check is run again with j = j-1. Otherwise, move the current
-            item(i) just before j.
+            """If the previous item of the list is smaller than the current one(i). If so, move the current
+            item(i) just at index j.
             """
-            if list[i] < list[j]:
-                if j - 1 >= 0 and list[i] < list[j - 1]:
-                    continue
+            if list[i].get_height() < list[j].get_height():
+                # if j - 1 >= 0 and list[i].get_height() < list[j - 1].get_height():
+                #     continue
+                list[i].set_color("red")
                 val = list.pop(i)
+                val.set_position(j, max(map(lambda c: c.get_height(), list)))
+                for k in range(j, len(list)):
+                    list[k].set_position(k + 1, max_height)
                 list.insert(j, val)
-                i -= 1  # Decrement i to account for the insertion before current i, thus ensuring that all items are checked
+                list[j].set_color("blue")
+                i -= 1
+            else:
                 break
         i += 1  # Increment i and continue the loop
     return list
--- turtle1.py
+++ turtle1.py
@@ -1,52 +1,87 @@
 import turtle
+from time import sleep
 
-t = turtle.Turtle()
-t.pencolor('black')
-name=turtle.Screen()
-turtle.screensize(bg="black")
-turtle.setup(width=1.0,height=1.0)
-name.title('Graphical view of insertion Sort ')
+from insertion_sort import sort
 
-def create(height,position,color='blue'):
-    #canvas 700x650
-    t.hideturtle()
-    t.speed('fastest')
-    t.penup()
-    if position < 35:
-        position = -700 + 31*position
-    else:
-        position = 700 - 31*position
-    t.goto(position,0)
-    draw(height,color)
+# t = turtle.Turtle()
+# t.pencolor("black")
+screen = turtle.Screen()
+screen.tracer(0)
+# name.screensize(bg="black", canvheight=650, canvwidth=700)
+# name.setup(width=1.0, height=1.0, starty=0, startx=0)
+screen.setup(height=650, width=700)
+screen.setworldcoordinates(llx=0, urx=700, lly=-650 // 2, ury=650 // 2)
+screen.bgcolor("black")
+screen.title("Graphical view of insertion Sort ")
 
-def draw(height,color):
-    t.begin_fill()
-    t.fillcolor('blue')
-    t.fillcolor(color)
-    t.pendown()
-    t.forward(20)
-    t.left(90)
-    t.forward(10*height)
-    t.left(90)
-    t.forward(20)
-    t.left(90)
-    t.forward(10*height)
-    t.left(90)
-    t.end_fill()
-    t.penup()
 
-def swap(c1,c2):
-    create(c1,i-1,'black')
-    create(c2,i-1)
-    create(c1,i)
+class Column:
+    def __init__(self, height: int, position: int, color: str = "blue"):
+        self._height = height
+        self._position = position
+        self._t = turtle.Turtle()
+        if color is not None:
+            self._t.color(color)
+        self._create(self._height, self._position)
 
-l = [5,8,6,30,10,25,17,23,15,9]
-pos = 0
+    def _create(self, height: int, position: int, max_height=None):
+        # canvas 700x650
+        self._t.hideturtle()
+        self._t.speed(0)
+        self._t.penup()
+        # if position < 35:
+        #     position = -700 + 31 * position
+        # else:
+        #     position = 700 - 31 * position
+        self._t.goto(position * 21, 0)
+        if max_height is not None:
+            color = self._t.color()[0]
+            self._t.color("black")
+            self._draw(max_height)
+            self._t.color(color)
+        self._draw(height)
 
-for item in l:
-    create(item,pos)
-    pos+=1
-i = 4
-swap(l[i-1],l[i])
-#πρώτα να βρούμε τρόπο να αλλάζει το χρώμα της επιλεγμένης μπάρας
-turtle.exitonclick()
+    def _draw(self, height: int):
+        self._t.begin_fill()
+        self._t.pendown()
+        self._t.forward(20)
+        self._t.left(90)
+        self._t.forward(10 * height)
+        self._t.left(90)
+        self._t.forward(20)
+        self._t.left(90)
+        self._t.forward(10 * height)
+        self._t.left(90)
+        self._t.end_fill()
+        self._t.penup()
+        self._t.screen.update()
+
+    def set_color(self, color):
+        self._t.color(color)
+        print(color)
+        self._draw(self._height)
+
+    def get_height(self):
+        return self._height
+
+    def get_position(self):
+        return self._position
+
+    def set_position(self, pos: int, max_height: int):
+        self._position = pos
+        self._create(self._height, self._position, max_height=max_height)
+
+
+l = [5, 8, 6, 30, 10, 25, 17, 23, 15, 9, 22, 18, 4, 1]
+columns = [Column(i, p) for p, i in enumerate(l)]
+sleep(5)
+sort(columns)
+# pos = 0
+
+# for item in l:
+#     create(item, pos)
+#     pos += 1
+# i = 4
+# swap(l[i - 1], l[i])
+# πρώτα να βρούμε τρόπο να αλλάζει το χρώμα της επιλεγμένης μπάρας
+screen.exitonclick()
