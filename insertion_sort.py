10da7b3a530afb4e9135efd63ef0b033f224f441
Georgepaxat <151514404+Georgepaxat@users.noreply.github.com>
2023-11-29 15:10:06 +0200

    remove useless info

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,16 +1,14 @@
-def sort(li: list):
+def sort(list: list):
     i=1
     n=1
     while True:
         try:
-            if li[i]<li[i-1]:   
-                li[i-1],li[i]=li[i],li[i-1]
-                print('step {}:{}'.format(n,li))
+            if list[i]<list[i-1]:   
+                list[i-1],list[i]=list[i],list[i-1]
                 n+=1
                 i=1   
             else:
                 i+=1
         except IndexError:
             break
-    return li
-sort(li=[9, 5, 4, 6, 7, 3, 10, 2, 8])
+    return list
