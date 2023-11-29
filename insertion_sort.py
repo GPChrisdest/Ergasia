95bc4595ef122e7f20986037ff226681b38e2994
Georgepaxat <151514404+Georgepaxat@users.noreply.github.com>
2023-11-29 14:52:52 +0200

    step by step sorting algo

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,16 +1,16 @@
-def sort(list: list):
-    i = 1  # Starting index
-    while i < len(list):  # Loop through the whole list
-        for j in range(i - 1, -1, -1):  # Loop through indices below current index
-            """If the previous item of the list is smaller than the current one(i), check if the one before that(j-1)
-            is lower as well. If so, continue so that the check is run again with j = j-1. Otherwise, move the current
-            item(i) just before j.
-            """
-            if list[i] < list[j]:
-                if j - 1 >= 0 and list[i] < list[j - 1]:
-                    continue
-                val = list.pop(i)
-                list.insert(j, val)
-                break
-        i += 1  # Increment i and continue the loop
-    return list
+def sort(li: list):
+    i=1
+    n=1
+    while True:
+        try:
+            if li[i]<li[i-1]:   
+                li[i-1],li[i]=li[i],li[i-1]
+                print('step {}:{}'.format(n,li))
+                n+=1
+                i=1   
+            else:
+                i+=1
+        except IndexError:
+            break
+    return li
+sort(li=[9, 5, 4, 6, 7, 3, 10, 2, 8])
