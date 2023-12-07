aed1e44c2f95d3b3dc5a6952229d824698392e39
Georgepaxat <151514404+Georgepaxat@users.noreply.github.com>
2023-12-07 16:09:09 +0200

    .....

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,10 +5,13 @@ def sort(list: list):
         val_height = val.get_height()
         j =i-1
         while j >= 0 and val_height < list[j].get_height():
+            list[j].set_color("red")  
+            list[j+1].set_color("red")
             list[j+1] = list[j]
             list[j].set_position(j+1, max_height)
             j-= 1
+            list[j].set_color("white")  
+            list[j+1].set_color("white")  
         list[j+1] = val
         val.set_position(j+1, max_height)
-        
     return list
